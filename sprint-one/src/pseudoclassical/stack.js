22e5351e7e7ab242fe5e3e7b0f434d1515537dd3
EricZH <letItBezhumphries>
2018-10-06 23:34:59 -0700

    "Complete pseudoclassical stack and queue"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+  this.storage = {};
+  this.numKey = 0;
+};
+
+Stack.prototype.push = function(value) {
+  this.numKey += 1;
+  this.storage[this.numKey] = value;
+};
+
+Stack.prototype.pop = function() {
+  let top = this.storage[this.size()];
+  delete this.storage[this.size()];
+  this.numKey -= 1;
+  return top;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
