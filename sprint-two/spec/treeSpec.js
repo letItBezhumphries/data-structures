b1f986025c7765bd3580e5e056cf6f6562222f8a
EricZH <letItBezhumphries>
2018-10-17 22:54:34 -0700

    started reimplementation of tree for Advanced Tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,10 +5,18 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  //advanced methods implemented
+  it('should have methods named "addChild" and "contains" and "removeFromParent" and "traverse"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
+    expect(tree.traverse).to.be.a('function');
+  });
+
+  //advanced properties added
+  it('should have properties named "value" and "parent"', function() {
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
   });
 
   it('should add children to the tree', function() {
@@ -32,6 +40,14 @@ describe('tree', function() {
     expect(tree.children[0].children[0].value).to.equal(6);
   });
 
+  // //our tree test
+  // it('should not be able to add children to a non-existent tree\'s child', function() {
+  //   tree.addChild(5);
+  //   tree.children[0].addChild(6);
+  //   expect(function() { tree.children[1].addChild(6); }).not.throws();
+  // });
+
+
   it('should correctly detect nested children', function() {
     tree.addChild(5);
     tree.addChild(6);
@@ -39,6 +55,37 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  /*    *     *    A D V A N C E D    C O N T E N T    *     *      */
+
+  //advanced test -- not sure if its implemented correctly??
+  it('ADVANCED TEST: should correctly disassociate the tree with its parent', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7); //add to node 5
+    tree.children[0].children[0].removeFromParent();
+    expect(tree.contains(7)).to.equal(false);
+    expect(tree.children[0].children[0]).to.equal(undefined);
+    tree.children[0].addChild(8);
+    tree.children[0].addChild(9);
+    tree.children[0].addChild(3);
+    tree.children[0].children[1].removeFromParent();
+    expect(tree.contains(9)).to.equal(false);
+    //?? check to see if childen array does not point to removed node  ??
+    expect(tree.children[0].children[1]).to.equal(3); //if there is only one child
+    expect(tree.children[0].children).to.eql([8, 3]);
+  });
+
+  it('AVANCED TEST: should correctly traverse through your tree/ children', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    tree.addChild(5);//root
+    tree.addChild(8);//child
+    tree.children[0].addChild(6);
+    tree.children[0].addChild(9);
+    tree.traverse(func);
+    expect(array).to.eql([11, 5, 6, 9, 8]);
   });
 
 });
